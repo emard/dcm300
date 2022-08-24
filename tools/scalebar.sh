8d9526124a3f1edff84a5aa7daf7e039d7bc51e0
Emard <vordah@gmail.com>
2022-08-24 12:44:15 +0200

    scalebar script

---
+++ tools/scalebar.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# usage:
+# ./scalebar.sh input.pnm output.pnm
+
+# edit -pointsize value to adjust scale
+
+convert \
+$1 \
+-background white \
+-pointsize 70 \
+-font DejaVu-Sans-Mono \
+label:'__  ▕          ▏  __\nmm 0▕░▒░▒░▒░▒░▒▏1 mm\n    ▕          ▏' \
+-gravity center \
+-append \
+$2
+
